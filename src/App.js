7ad3c21f8d27cb4b670193453463225a040d368c
edielam <edguard738@gmail.com>
2024-08-02 19:02:45 -0400

    fixed routing

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Home from "./Home";
 
 const App = () => {
   //eslint-disable-next-line
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1000); // Adjust the time as needed
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setLoading(false), 1000); // Adjust the time as needed
+  //   return () => clearTimeout(timer);
+  // }, []);
   return (
     <Router>
       <Routes>
